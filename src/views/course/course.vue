09331c64fd80f861665467916e5f04fdff38cd3c
李强 <1484190946@qq.com>
2018-12-18 16:05:06 +0800

    complete base table fetch

--- src/views/course/course.vue
+++ src/views/course/course.vue
@@ -1,58 +1,100 @@
 <template>
     <div style="margin: 10px">
         <div>
-            <el-row :gutter="5">
-                <el-col :span="12">
-                    <el-input v-model="loginName"></el-input>
-                </el-col>
+            <el-row :gutter="5" style="margin-bottom: 16px">
                 <el-col :span="5">
+                    <el-input v-model="searchKeyword"></el-input>
+                </el-col>
+                <el-col :span="19">
                     <el-button type="primary">
                         <i class="el-icon-search"></i>
                         <span>查找</span>
+                    </el-button>
+                    <el-button type="primary" @click="newModal=true">
+                        <i class="el-icon-plus"></i>
+                        新建
                     </el-button>
                 </el-col>
             </el-row>
         </div>
 
         <div>
-
             <ul style="list-style: none;padding: 0">
-                <li style="margin-top:10px;margin-bottom:20px">
-                    <el-button type="primary" icon="el-icon-plus" @click="newModal=true">新建</el-button>
-                    <el-button type="success" icon="el-icon-edit">修改</el-button>
-                    <el-button type="danger" icon="el-icon-delete">删除</el-button>
-                </li>
                 <li>
                     <div>
-                        <el-table ref="multipleTable" :data="tableData3" tooltip-effect="dark" style="width: 100%"
+                        <el-table border ref="multipleTable" :data="courseTableData" tooltip-effect="dark" style="width: 100%"
                             @selection-change="handleSelectionChange">
-                            <el-table-column type="selection" width="55">
-                            </el-table-column>
-                            <el-table-column label="课程编号" width="150">
-                                <template slot-scope="scope">{{ scope.row.date }}</template>
-                            </el-table-column>
-                            <el-table-column prop="name" label="课程名称" width="250">
-                            </el-table-column>
-                            <el-table-column label="学分" width="150">
-                            </el-table-column>
-                            <el-table-column label="学时" width="150">
-                            </el-table-column>
-                            <el-table-column label="课程性质描述" width="300">
-                            </el-table-column>
-                            <el-table-column label="课程性质" width="200">
-                                <template slot-scope="scope">
-                                    <el-radio v-model="tableData3.radio" label="1">必修</el-radio>
-                                    <el-radio v-model="tableData3.radio" label="2">非必修</el-radio>
-                                </template>
+                            <el-table-column type="selection" width="50" align="center">
                             </el-table-column>
 
+                            <el-table-column label="课程编号" width="150" prop="course.id">
+
+                            </el-table-column>
+
+                            <el-table-column prop="course.name" label="课程名称" width="250">
+                            </el-table-column>
+
+                            <el-table-column prop="course.point" label="学分" width="150">
+                            </el-table-column>
+
+                            <el-table-column prop="course.hour" label="学时" width="150">
+                            </el-table-column>
+
+                            <el-table-column prop="courseNature.name" label="课程性质" width="200">
+                            </el-table-column>
+
+                            <el-table-column prop="courseNature.required" label="必修">
+                                <template slot-scope="scope">
+                                    <el-switch
+                                            v-model="scope.row.courseNature.required"
+                                            active-text="必修"
+                                            disabled>
+                                    </el-switch>
+                                </template>
+                            </el-table-column>
+                            <el-table-column prop="courseNature.description" label="课程性质描述" width="300">
+                            </el-table-column>
+                            <el-table-column align="center" width="200" label="操作">
+
+
+                                <template slot-scope="scope" >
+                                    <!--配置角色按钮-->
+                                    <el-tooltip class="item" effect="dark" content="配置角色" placement="top">
+                                        <el-button size="mini"
+                                                   type="info"
+                                                   @click="onRoleSetButtonClick(scope.$index,tableData)"
+                                                   circle>
+                                            <i class="fas fa-users-cog"></i>
+                                        </el-button>
+                                    </el-tooltip>
+
+                                    <el-tooltip class="item" effect="dark" content="重置密码" placement="top">
+                                        <el-button size="mini"
+                                                   type="warning"
+                                                   @click="resetPass(scope.$index,tableData)"
+                                                   circle>
+                                            <i class="fas fa-keyboard"></i>
+                                        </el-button>
+                                    </el-tooltip>
+
+                                    <el-tooltip class="item" effect="dark" content="删除用户" placement="top">
+                                        <el-button
+                                                size="mini"
+                                                type="danger"
+                                                icon="el-icon-delete"
+                                                @click="deleteUser(scope.$index,tableData)"
+                                                circle>
+                                        </el-button>
+                                    </el-tooltip>
+                                </template>
+                            </el-table-column>
                         </el-table>
                     </div>
                 </li>
                 <li>
                     <div class="block">
                         <span class="demonstration"></span>
-                        <el-pagination layout="prev, pager, next" :total="1000">
+                        <el-pagination layout="->,prev, pager, next" :total="total">
                         </el-pagination>
                     </div>
                 </li>
@@ -69,51 +111,49 @@
 
         data() {
             return {
-                tableData3: [{
-                    date: '2016-05-03',
-                    name: '王小虎',
-                    address: '1518',
-                    radio: '1'
-                }, {
-                    date: '2016-05-02',
-                    name: '王小虎',
-                    address: '1518',
-                    radio: '1'
-                }, {
-                    date: '2016-05-04',
-                    name: '王小虎',
-                    address: '1518',
-                    radio: '1'
-                }, {
-                    date: '2016-05-01',
-                    name: '王小虎',
-                    address: '1518',
-                    radio: '1'
-                }, {
-                    date: '2016-05-08',
-                    name: '王小虎',
-                    address: '1518',
-                    radio: '1'
-                }, {
-                    date: '2016-05-06',
-                    name: '王小虎',
-                    address: '1518',
-                    radio: '1'
-                }, {
-                    date: '2016-05-07',
-                    name: '王小虎',
-                    address: '1518',
-                    radio: '1'
-                }],
+                searchKeyword:'',
+                total:10,
+                pageInfo:{
+                    pageNum:1,
+                    pageSize:10
+                },
+                courseTableData:[{
+                    course: {
+                        id: 2,
+                        couId: null,
+                        name: "计算机网络",
+                        point: 2,
+                        hour: 48
+                    },
+                    courseNature: {
+                        id: 1,
+                        name: "专业必修课",
+                        description: ".",
+                        required: true
+                    }
+                },],
                 multipleSelection: [],
-
-
-
-
-
             };
-
         },
+        mounted(){
+            this.fetchCourseData();
+        },
+        methods:{
+            fetchCourseData(){
+                this.axios({
+                    url:'/course/page',
+                    method:'get',
+                    data:this.pageInfo
+                }).then((res)=>{
+                    console.log(res.data.data);
+                    this.courseTableData=res.data.data.list;
+                    this.total = res.data.data.total;
+                })
+            },
+            handleSelectionChange(selection){
+
+            }
+        }
 
     }
 </script>
